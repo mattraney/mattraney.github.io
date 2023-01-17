9e1421624cf292b9ac6602611a747843af8d206b
mattraney <121915412+mattraney@users.noreply.github.com>
2023-01-17 10:35:39 -0500

    Add files via upload

--- assets/js/datatables.js
+++ assets/js/datatables.js
@@ -4,17 +4,42 @@ window.addEventListener('DOMContentLoaded', event => {
 
     const datatablesSimple1 = document.getElementById('datatablesOrders');
     if (datatablesSimple1) {
-        new simpleDatatables.DataTable(datatablesOrders);
+        new simpleDatatables.DataTable(datatablesOrders, {
+            labels: {
+                placeholder: "Search",
+                perPage: "{select} per page",
+                noRows: "No orders to display",
+                info: "Showing {start} to {end} of {rows} orders",
+                noResults: "No orders found",
+            }
+        });
     }
 
     const datatablesSimple2 = document.getElementById('datatablesWH');
     if (datatablesSimple2) {
-        new simpleDatatables.DataTable(datatablesWH);
-    }    
+        new simpleDatatables.DataTable(datatablesWH, {
+            labels: {
+                placeholder: "Search",
+                perPage: "{select} per page",
+                noRows: "No warehouses to display",
+                info: "Showing {start} to {end} of {rows} warehouses",
+                noResults: "No warehouses found",
+            }
+        });
+    }
 
     const datatablesSimple3 = document.getElementById('datatablesPriceMatch');
     if (datatablesSimple3) {
-        new simpleDatatables.DataTable(datatablesPriceMatch);
+        new simpleDatatables.DataTable(datatablesPriceMatch, {
+            sortable: false,
+            labels: {
+                placeholder: "Search",
+                perPage: "{select} per page",
+                noRows: "No opportunities to display",
+                info: "Showing {start} to {end} of {rows} opportunities",
+                noResults: "No opportunities found",
+            }
+        });
     }
 
 });
