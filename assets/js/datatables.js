98f267e92c01bc932a91e87a6d5b7fb3eeeacded
mattraney <121915412+mattraney@users.noreply.github.com>
2023-01-17 08:08:50 -0500

    Add files via upload

--- assets/js/datatables.js
+++ assets/js/datatables.js
@@ -2,13 +2,19 @@ window.addEventListener('DOMContentLoaded', event => {
     // Simple-DataTables
     // https://github.com/fiduswriter/Simple-DataTables/wiki
 
-    const datatablesSimple = document.getElementById('datatablesSimple');
-    if (datatablesSimple) {
-        new simpleDatatables.DataTable(datatablesSimple);
+    const datatablesSimple1 = document.getElementById('datatablesOrders');
+    if (datatablesSimple1) {
+        new simpleDatatables.DataTable(datatablesOrders);
     }
 
     const datatablesSimple2 = document.getElementById('datatablesWH');
     if (datatablesSimple2) {
         new simpleDatatables.DataTable(datatablesWH);
     }    
+
+    const datatablesSimple3 = document.getElementById('datatablesPriceMatch');
+    if (datatablesSimple3) {
+        new simpleDatatables.DataTable(datatablesPriceMatch);
+    }
+
 });
