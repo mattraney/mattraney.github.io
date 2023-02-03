2d289996f2d6308d7f38582962b656097612fbcf
mattraney <121915412+mattraney@users.noreply.github.com>
2023-02-03 13:29:25 -0500

    Add files via upload

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -1,8 +1,8 @@
-var setEndDate1 = "Jan 30, 2023 15:00:00";
-var setEndDate2 = "Jan 30, 2023 15:30:00";
-var setEndDate3 = "Jan 30, 2023 16:00:00";
-var setEndDate4 = "Jan 31, 2023 16:30:00";
-var setEndDate5 = "Jan 31, 2023 15:00:00";
+var setEndDate1 = "Feb 20, 2023 15:00:00";
+var setEndDate2 = "Feb 20, 2023 15:30:00";
+var setEndDate3 = "Feb 20, 2023 16:00:00";
+var setEndDate4 = "Feb 21, 2023 16:30:00";
+var setEndDate5 = "Feb 21, 2023 15:00:00";
 
 function startCountDownDate(dateVal) {
   var countDownDate = new Date(dateVal).getTime();
